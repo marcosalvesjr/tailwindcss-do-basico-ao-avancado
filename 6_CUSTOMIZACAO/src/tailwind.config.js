08a4877f887a94125387968a2c548fb00214aba2
Marcos Alves <mrcaurelio95@gmail.com>
2025-02-01 21:41:17 -0300

    customização

---
+++ 6_CUSTOMIZACAO/src/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"], // Adapte conforme sua estrutura de arquivos
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
